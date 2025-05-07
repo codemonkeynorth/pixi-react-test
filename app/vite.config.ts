a69273a5f3d78601008eeb740b7411182f6d54d7
codemonkeynorth <joe@codemonkeynorth.co.uk>
2025-05-07 21:06:46 +0100

    visualizer

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,10 +1,11 @@
+import { visualizer } from "rollup-plugin-visualizer"
 import { defineConfig } from "vite"
 
 import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer()],
   build: {
     rollupOptions: {
       output: {
